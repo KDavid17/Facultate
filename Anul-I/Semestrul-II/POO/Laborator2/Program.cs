ff13d212afcd5c46705eb8403e16ce8b9e6152cd
Kiss David <kiss_david17@yahoo.com>
2021-03-04 12:00:52 +0200

    Am adaugat Laborator2.

---
+++ Anul-I/Semestrul-II/POO/Laborator2/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n Introduceti '+' , '-' , '*' , '/' , '^' in functie de operatia dorita: ");
+            char sign = Console.ReadKey().KeyChar;
+
+            switch (sign)
+            {
+                case '+':
+
+                    Addition();
+
+                    break;
+
+                case '-':
+
+                    Subtraction();
+
+                    break;
+
+                case '*':
+
+                    Multiplication();
+
+                    break;
+
+                case '/':
+
+                    Division();
+
+                    break;
+
+                case '^':
+
+                    PowerRaising();
+
+                    break;
+
+                default:
+
+                    Console.Write("\n\n Nu ati introdus date procesabile conform cerintei. \n\n");
+
+                    break;
+            }
+        }
+
+        private static void PowerRaising()
+        {
+            Console.Write("\n\n Introduceti numaratorul si numitorul numarului. \n\n Numarator: ");
+            int numerator = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int denominator = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Introduceti puterea la care sa fie ridicat numarul: ");
+            int exponent = int.Parse(Console.ReadLine());
+
+            if (denominator == 0)
+            {
+                Console.Write("\n Calcul imposibil din cauza impartirii la 0. \n\n");
+            }
+            else if (numerator == 0)
+            {
+                Console.Write($"\n (0, {denominator}) ^ {exponent} = 0 \n\n");
+            }
+            else
+            {
+                if (exponent != 0)
+                {
+                    if (exponent < 0)
+                    {
+                        Rational numb = new Rational(denominator, numerator);
+
+                        Console.Write($"\n ({numerator}, {denominator}) ^ ({exponent}) = {numb.Raise(-exponent)} \n\n");
+                    }
+                    else
+                    {
+                        Rational numb = new Rational(numerator, denominator);
+
+                        Console.Write($"\n ({numerator}, {denominator}) ^ {exponent} = {numb.Raise(exponent)} \n\n");
+                    }
+                }
+                else
+                {
+                    Console.Write($"\n ({numerator}, {denominator}) ^ 0 = 1 \n\n");
+
+                    return;
+                }
+            }
+        }
+
+        private static void Division()
+        {
+            Console.Write("\n\n Introduceti numaratorul si numitorul primului numar. \n\n Numarator: ");
+            int numerator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int denominator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Introduceti numaratorul si numitorul celui de al doilea numar. \n\n Numarator: ");
+            int denominator2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int numerator2 = int.Parse(Console.ReadLine());
+
+            if (denominator1 == 0 || denominator2 == 0 || numerator2 == 0)
+            {
+                Console.Write("\n Calcul imposibil din cauza impartirii la 0. \n\n");
+            }
+            else if (numerator1 == 0)
+            {
+                Console.Write($"\n (0, {denominator1}) / ({numerator2}, {denominator2}) = 0 \n\n");
+            }
+            else
+            {
+                Rational numb1 = new Rational(numerator1, denominator1);
+                Rational numb2 = new Rational(numerator2, denominator2);
+
+                Console.Write($"\n ({numerator1}, {denominator1}) / ({denominator2}, {numerator2}) = {numb1.Multiply(numb2)} \n\n");
+            }
+        }
+
+        private static void Multiplication()
+        {
+            Console.Write("\n\n Introduceti numaratorul si numitorul primului numar. \n\n Numarator: ");
+            int numerator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int denominator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Introduceti numaratorul si numitorul celui de al doilea numar. \n\n Numarator: ");
+            int numerator2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int denominator2 = int.Parse(Console.ReadLine());
+
+            if (denominator1 == 0 || denominator2 == 0)
+            {
+                Console.Write("\n Calcul imposibil din cauza impartirii la 0. \n\n");
+            }
+            else if (numerator1 == 0 || numerator2 == 0)
+            {
+                Console.Write($"\n ({numerator1}, {denominator1}) * ({numerator2}, {denominator2}) = 0 \n\n");
+            }
+            else
+            {
+                Rational numb1 = new Rational(numerator1, denominator1);
+                Rational numb2 = new Rational(numerator2, denominator2);
+
+                Console.Write($"\n ({numerator1}, {denominator1}) * ({numerator2}, {denominator2}) = {numb1.Multiply(numb2)} \n\n");
+            }
+        }
+
+        private static void Subtraction()
+        {
+            Console.Write("\n\n Introduceti numaratorul si numitorul primului numar. \n\n Numarator: ");
+            int numerator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int denominator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Introduceti numaratorul si numitorul celui de al doilea numar. \n\n Numarator: ");
+            int numerator2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int denominator2 = int.Parse(Console.ReadLine());
+
+            if (denominator1 == 0 || denominator2 == 0)
+            {
+                Console.Write("\n Calcul imposibil din cauza impartirii la 0. \n\n");
+            }
+            else if (numerator1 == 0)
+            {
+                Console.Write($"\n ({numerator1}, {denominator1}) - ({numerator2}, {denominator2}) = ({-numerator2}, {denominator2}) \n\n");
+            }
+            else if (numerator2 == 0)
+            {
+                Console.Write($"\n ({numerator1}, {denominator1}) - ({numerator2}, {denominator2}) = ({numerator1}, {denominator1}) \n\n");
+            }
+            else
+            {
+                Rational numb1 = new Rational(numerator1, denominator1);
+                Rational numb2 = new Rational(numerator2, denominator2);
+
+                Console.Write($"\n ({numerator1}, {denominator1}) - ({numerator2}, {denominator2}) = {numb1.Subtract(numb2)} \n\n");
+            }
+        }
+
+        private static void Addition()
+        {
+            Console.Write("\n\n Introduceti numaratorul si numitorul primului numar. \n\n Numarator: ");
+            int numerator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int denominator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Introduceti numaratorul si numitorul celui de al doilea numar. \n\n Numarator: ");
+            int numerator2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Numitor: ");
+            int denominator2 = int.Parse(Console.ReadLine());
+
+            if (denominator1 == 0 || denominator2 == 0)
+            {
+                Console.Write("\n Calcul imposibil din cauza impartirii la 0. \n\n");
+            }
+            else if (numerator1 == 0)
+            {
+                Console.Write($"\n ({numerator1}, {denominator1}) + ({numerator2}, {denominator2}) = ({numerator2}, {denominator2}) \n\n");
+            }
+            else if (numerator2 == 0)
+            {
+                Console.Write($"\n ({numerator1}, {denominator1}) + ({numerator2}, {denominator2}) = ({numerator1}, {denominator1}) \n\n");
+            }
+            else
+            {
+                Rational numb1 = new Rational(numerator1, denominator1);
+                Rational numb2 = new Rational(numerator2, denominator2);
+
+                Console.Write($"\n ({numerator1}, {denominator1}) + ({numerator2}, {denominator2}) = {numb1.Add(numb2)} \n\n");
+            }
+        }
+    }
+}
---
+++ Anul-I/Semestrul-II/POO/Laborator2/Rational.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+namespace Laborator2
+{
+    internal class Rational
+    {
+        public int numerator, denominator;
+
+        public Rational(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public override string ToString()
+        {
+            return "(" + numerator + ", " + denominator + ")"; 
+        }
+
+        private int lcm(int a, int b)
+        {
+            return a * b / gcd(a, b);
+        }
+
+        private int gcd(int a, int b)
+        {
+            int c;
+
+            while(b != 0)
+            {
+                c = a % b;
+                a = b;
+                b = c;
+            }
+
+            return a;
+        }
+
+        private void Simplify(ref int numb1, ref int numb2)
+        {
+            int divisor = gcd(numb1, numb2);
+
+            numb1 /= divisor;
+            numb2 /= divisor;
+        }
+
+        public Rational Add(Rational numb2)
+        {
+            Simplify(ref numerator, ref denominator);
+            Simplify(ref numb2.numerator, ref numb2.denominator);
+
+            if (denominator != numb2.denominator)
+            {
+                if (denominator % numb2.denominator != 0 && numb2.denominator % denominator != 0)
+                {
+                    int multiple = lcm(denominator, numb2.denominator);
+                    int numeratorResult = numerator * (multiple / denominator) + 
+                                            numb2.numerator * (multiple / numb2.denominator);
+                    int divisor = gcd(numeratorResult, multiple);
+
+                    return new Rational(numeratorResult / divisor, multiple / divisor);
+                }
+                else
+                {
+                    if (denominator < numb2.denominator)
+                    {
+                        int numeratorResult = numerator * (numb2.denominator / denominator) + numb2.numerator;
+                        int divisor = gcd(numeratorResult, numb2.denominator);
+
+                        return new Rational(numeratorResult / divisor, numb2.denominator / divisor);
+                    }
+                    else
+                    {
+                        int numeratorResult = numerator + numb2.numerator * (denominator / numb2.denominator);
+                        int divisor = gcd(numeratorResult, denominator);
+
+                        return new Rational(numeratorResult / divisor, denominator / divisor);
+                    }
+                }
+            }
+            else
+            {
+                int numeratorResult = numerator + numb2.numerator;
+                int divisor = gcd(numeratorResult, denominator);
+
+                return new Rational(numeratorResult / divisor, denominator / divisor);
+            }
+        }  
+
+        public Rational Subtract(Rational numb2)
+        {
+            Simplify(ref numerator, ref denominator);
+            Simplify(ref numb2.numerator, ref numb2.denominator);
+
+            if (denominator != numb2.denominator)
+            {
+                if (denominator % numb2.denominator != 0 && numb2.denominator % denominator != 0)
+                {
+                    int multiple = lcm(denominator, numb2.denominator);
+                    int numeratorResult = numerator * (multiple / denominator) -
+                                            numb2.numerator * (multiple / numb2.denominator);
+                    int divisor = gcd(numeratorResult, multiple);
+
+                    return new Rational(numeratorResult / divisor, multiple / divisor);
+                }
+                else
+                {
+                    if (denominator < numb2.denominator)
+                    {
+                        int numeratorResult = numerator * (numb2.denominator / denominator) - numb2.numerator;
+                        int divisor = gcd(numeratorResult, numb2.denominator);
+
+                        return new Rational(numeratorResult / divisor, numb2.denominator / divisor);
+                    }
+                    else
+                    {
+                        int numeratorResult = numerator - numb2.numerator * (denominator / numb2.denominator);
+                        int divisor = gcd(numeratorResult, denominator);
+
+                        return new Rational(numeratorResult / divisor, denominator / divisor);
+                    }
+                }
+            }
+            else
+            {
+                int numeratorResult = numerator - numb2.numerator;
+                int divisor = gcd(numeratorResult, denominator);
+
+                return new Rational(numeratorResult / divisor, denominator / divisor);
+            }
+        }
+        
+        public Rational Multiply(Rational numb2)
+        {
+            Simplify(ref numerator, ref denominator);
+            Simplify(ref numb2.numerator, ref numb2.denominator);
+            Simplify(ref numerator, ref numb2.denominator);
+            Simplify(ref denominator, ref numb2.numerator);
+
+            Console.Write($"\n\n {numerator}, {denominator}     {numb2.numerator}, {numb2.denominator}\n\n");
+
+            int numeratorResult = numerator * numb2.numerator;
+            int denominatorResult = denominator * numb2.denominator;
+            int divisor = gcd(numeratorResult, denominatorResult);
+
+            return new Rational(numeratorResult / divisor, denominatorResult / divisor);
+        }
+
+        public Rational Raise(int exponent)
+        {
+            Simplify(ref numerator, ref denominator);
+
+            Rational result = new Rational(numerator, denominator);
+
+            for (int i = 2; i <= exponent; i++)
+            {
+                result = result.Multiply(new Rational(numerator, denominator));
+            }
+
+            return result;
+        }
+    }
+}
